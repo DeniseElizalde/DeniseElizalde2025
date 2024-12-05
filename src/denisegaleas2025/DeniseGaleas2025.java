21a013faa674766883fdaad44ffed03899aa6906
DeniseElizalde <denise.elizalde@espoch.edu.ec>
2024-12-05 11:38:46 -0500

    mi primer commit

---
+++ src/denisegaleas2025/DeniseGaleas2025.java
@@ -0,0 +1,14 @@
+
+package denisegaleas2025;
+
+public class DeniseGaleas2025 {
+
+    public static void main(String[] args) {
+        
+        private Calculadora = new caculadora;
+        
+}
+        
+        
+    
+    }
---
+++ src/ec/edu/espoch/DeniseGaleas2025/Calculadoraa.java
@@ -0,0 +1,66 @@
+
+package ec.edu.espoch.DeniseGaleas2025;
+
+/**
+ *
+ * @author Dell
+ */
+public class Calculadoraa {
+    private int suma;
+    private int deshacer;
+    private int rehacer;
+    private int resultado;
+    private int salir;
+
+    public Calculadoraa(int suma, int deshacer, int rehacer, int resultado, int salir) {
+        this.suma = suma;
+        this.deshacer = deshacer;
+        this.rehacer = rehacer;
+        this.resultado = resultado;
+        this.salir = salir;
+    }
+
+    public int getSuma() {
+        return suma;
+    }
+
+    public void setSuma(int suma) {
+        this.suma = suma;
+    }
+
+    public int getDeshacer() {
+        return deshacer;
+    }
+
+    public void setDeshacer(int deshacer) {
+        this.deshacer = deshacer;
+    }
+
+    public int getRehacer() {
+        return rehacer;
+    }
+
+    public void setRehacer(int rehacer) {
+        this.rehacer = rehacer;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    public int getSalir() {
+        return salir;
+    }
+
+    public void setSalir(int salir) {
+        this.salir = salir;
+    }
+    
+
+    
+     
+}
